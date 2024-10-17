e033a39eb54a23cda43de8a906ce4cb4a1793d85
jeddy2018 <110876410+jeddy2018@users.noreply.github.com>
2024-10-17 15:03:05 -0500

    black

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,6 +1,7 @@
 """
 Test custom Django management commands.
 """
+
 from unittest.mock import patch
 
 from psycopg2 import OperationalError as Psycop2Error
@@ -9,25 +10,30 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
-@patch('core.management.commands.wait_for_db.Command.check') #replace the check method of the Command class with a mock object
+
+@patch(
+    "core.management.commands.wait_for_db.Command.check"
+)
 class CommandTest(SimpleTestCase):
     """Test commands."""
-    
+
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready."""
-        patched_check.return_value = True #indicates that when the check method is called, it will return True, simulating a ready database.
+        patched_check.return_value = True
 
-        call_command('wait_for_db') #simulates calling the wait_for_db command.
+        call_command("wait_for_db")
 
-        patched_check.assert_called_once_with(databases=['default']) #checks that the check method was called exactly once with the specified argument (database=['default'])
+        patched_check.assert_called_once_with(
+            databases=["default"]
+        )
 
-    @patch('time.sleep')
+    @patch("time.sleep")
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError."""
         patched_check.side_effect = [Psycop2Error] * 2 + \
             [OperationalError] * 3 + [True]
-        
-        call_command('wait_for_db')
+
+        call_command("wait_for_db")
 
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(databases=['default'])
+        patched_check.assert_called_with(databases=["default"])
