a2e7d30ff67ebb4f17e74bce25993e704753649b
jeddy2018 <110876410+jeddy2018@users.noreply.github.com>
2024-10-18 10:47:14 -0500

    added user admin

--- app/core/models.py
+++ app/core/models.py
@@ -13,7 +13,7 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """Create, save, and return a new user"""
         if(not email):
-            raise ValueError
+            raise ValueError("User must have an email address.")
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
@@ -24,7 +24,7 @@ class UserManager(BaseUserManager):
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
-        user.save(self._db)
+        user.save(using=self._db)
 
         return user
 
